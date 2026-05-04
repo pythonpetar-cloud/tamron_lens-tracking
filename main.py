d0a5c9d3943e35f498bad953c1015afdd36b8c9f
pythonpetar-cloud <pythonpetar@gmail.com>
2026-05-04 13:53:16 +0200

    Simplify Chrome driver initialization
    Removed remote debugging port and binary location settings.

--- main.py
+++ main.py
@@ -57,12 +57,8 @@ class KupujemProdajemBot:
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.binary_location = "/usr/bin/chromium-browser"
-
-    service = Service("/usr/bin/chromedriver")
-    self.driver = webdriver.Chrome(service=service, options=chrome_options)
+    self.driver = webdriver.Chrome(options=chrome_options)
     self.wait = WebDriverWait(self.driver, 35)
 
     def parse_price(self, price_text: str) -> tuple[float, str] | None:
