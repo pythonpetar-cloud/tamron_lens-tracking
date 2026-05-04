0550d11ee756764d961cb46c3a35fd717da04285
pythonpetar-cloud <pythonpetar@gmail.com>
2026-05-04 13:47:18 +0200

    Enhance Chrome options for headless mode
    Added remote debugging and window size options for Chrome.

--- main.py
+++ main.py
@@ -57,9 +57,11 @@ class KupujemProdajemBot:
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.binary_location = "/usr/bin/chromium-browser"
-    
-    service = webdriver.chrome.service.Service("/usr/bin/chromedriver")
+
+    service = Service("/usr/bin/chromedriver")
     self.driver = webdriver.Chrome(service=service, options=chrome_options)
     self.wait = WebDriverWait(self.driver, 35)
 
